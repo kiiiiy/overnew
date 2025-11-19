aaac387d6fc0522c20d35d2112e82fd0ac021bc9
yslee <yeongseo6142@naver.com>
2025-11-20 03:25:36 +0900

    모델 설정

--- overnew_prj/users/models.py
+++ overnew_prj/users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser  # BaseUserManager 필요 없음, 지워도 됨
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.CharField(max_length=50, unique=True)
+    nickname = models.CharField(max_length=15)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=2)
+
+    def __str__(self):
+        return self.username
+    
+
+class Following(models.Model):
+    following_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='follow_from')
+    user2 = models.ForeignKey('User', on_delete=models.CASCADE, related_name='follow_to')
+
+    class Meta:
+        unique_together = ('user', 'user2')
